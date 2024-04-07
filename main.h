b7780d7522eb4472291fa9c8dc45783f4a5d4ede
AlekTagil <alek_zrg@mail.ru>
2024-04-07 17:01:47 +0500

    add headers and memory callback function

---
+++ main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+typedef struct  {
+    char *str;
+    size_t size;
+} MemoryStruct;
---
+++ write_memory_callback.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+static size_t WriteMemoryCallback(void *content, size_t size, size_t nmemb, void *userp) {
+    size_t real_size = size * nmemb;
+    MemoryStruct *mem = (MemoryStruct *) userp;
+
+    char *ptr = realloc(mem->str, mem->size + real_size + 1);
+
+    if (!ptr) {
+        return 0;
+    }
+
+    mem->str = ptr;
+    memcpy(&(mem->str[mem->size]), content, real_size);
+    mem->size += real_size;
+    mem->str[mem->size] = 0;
+    return real_size;
+}
