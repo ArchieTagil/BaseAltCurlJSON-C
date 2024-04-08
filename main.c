91f81ff165ab1727b240c130a10171f8fa8aaca8
AlekTagil <alek_zrg@mail.ru>
2024-04-08 12:27:16 +0500

    correct api links

--- main.c
+++ main.c
@@ -5,8 +5,17 @@ int main() {
     chunk.str = malloc(1);
     chunk.size = 0;
 
-    get_user_input();
-    get_remote_data("https://petstore.swagger.io/v2/store/inventory", &chunk);
+    int branch_id = get_user_input();
+    switch (branch_id) {
+    case 1:
+        get_remote_data("https://rdb.altlinux.org/api/export/branch_binary_packages/sisyphus", &chunk);
+        break;
+    case 2:
+        get_remote_data("https://rdb.altlinux.org/api/export/branch_binary_packages/p10", &chunk);
+        break;
+    default:
+        return 1;
+    }
 
     json_object *root = json_tokener_parse(chunk.str);
     printf("\n%s\n", json_object_to_json_string_ext(root, JSON_C_TO_STRING_PRETTY));
